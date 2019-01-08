ab1e45f7bd440afd3ca10b66b716ff591d10fea8
Olli Mahlamäki <olli.mahlamaki@gmail.com>
2019-01-08 21:05:47 +0200

    Save state to mongo

---
+++ src/mongo.js
@@ -0,0 +1,67 @@
+import { MongoClient } from "mongodb";
+
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+const dbName = "drawOta1";
+
+const client = new MongoClient(url);
+
+let id = null;
+
+const createClient = callback => {
+  client.connect(function(err, client) {
+    console.log("connected to mongo");
+    const db = client.db(dbName);
+    const pixelsCol = db.collection("pixels");
+
+    const save = data => {
+      if (id) {
+        pixelsCol.updateOne(
+          {
+            _id: id
+          },
+          {
+            $set: {
+              pixels: data
+            }
+          }
+        );
+      } else {
+        pixelsCol.insertOne(
+          {
+            pixels: data
+          },
+          (err, result) => {
+            console.log("insert", result);
+            if (result && result.insertedId) {
+              id = result.insertedId;
+            }
+          }
+        );
+      }
+    };
+
+    const restore = cb => {
+      pixelsCol.findOne({}).then(
+        result => {
+            console.log("restored", result)
+          if (result && result.pixels) {
+            id = result._id;
+            console.log("restored with id", id);
+            cb(result.pixels);
+          } else {
+            cb(null);
+          }
+        },
+        () => cb(null)
+      );
+    };
+
+    callback({
+      save,
+      restore
+    });
+  });
+};
+
+export default createClient;
