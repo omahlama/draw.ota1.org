6462de211c54986ec6ffcafa1f3c7c623b3a2387
Olli Mahlamäki <olli.mahlamaki@gmail.com>
2019-01-16 13:36:16 +0200

    Use webpack for css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style.css';
 import Shake from 'shake.js';
 import createClient from './client';
 import colors from './colors';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,14 +17,33 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+        ],
+      },
     ],
   },
   // Plugins
   plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
     new htmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
     }),
-    CopyWebpackPlugin([{ from: 'src/style.css', to: '.' }]),
   ],
 };
